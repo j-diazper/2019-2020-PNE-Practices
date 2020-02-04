1bdd28255de3e670113d88d3e261f128b53420e2
j-diazper <jdiazper2001@gmail.com>
2020-02-04 16:38:59 +0100

    just vibin doe

--- session 2/fibo-sumN.py
+++ session 2/fibo-sumN.py
@@ -1,11 +1,14 @@
 def fibon(n):
     f0 = 0
     f1 = 1
+    sum=f0+f1
     for i in range(0, n - 1):
         n_val = f0 + f1
         f0 = f1
         f1 = n_val
-    return n_val
-
-
+        sum=sum+n_val
+    return sum
 print("sum of the 5th terms: ", fibon(5))
+print("sum of the 10th terms: ", fibon(10))
+
+
