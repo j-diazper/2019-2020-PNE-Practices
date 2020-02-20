b37cf44c565bab5fe47280c05a4cb951440e1740
j-diazper <jdiazper2001@gmail.com>
2020-02-20 11:50:30 +0100

    changes

---
+++ Practice 1/P1/Ex7.py
@@ -0,0 +1,13 @@
+from Seq1 import Seq
+# -- Create a Null sequence
+s1 = Seq()
+
+# -- Create a valid sequence
+s2 = Seq("ACTGA")
+
+# -- Create an invalid sequence
+s3 = Seq("Invalid sequence")
+seq_list=[s1,s2,s3]
+BASE_list=["A","C","T","G"]
+for i in seq_list:
+    print("Sequence",(seq_list.index(i)+1),":",": (Length: ",i.len(),")",i,"\n",i.count(BASE_list),"\n","Rev: ",i.reverse())
