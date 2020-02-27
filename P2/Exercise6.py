97784b601afede631c34c44c50b5f474138424ca
j-diazper <jdiazper2001@gmail.com>
2020-02-27 14:54:49 +0100

    vibin doe

--- P2/Exercise6.py
+++ P2/Exercise6.py
@@ -3,18 +3,16 @@ from pathlib import Path
 
 def dividing_seq(FILENAME):
     bodystr=""
-    new_list= []
-    total_list=[]
     file_contents = Path(FILENAME).read_text()
     lines = file_contents.split("\n")
     body = lines[1:]
     bodystr=bodystr.join(body).replace(",","")
-    for i in bodystr:
-        new_list.append(i)
-        if len(new_list)==10:
-            total_list.append(new_list)
-            continue
-    return total_list
+    split_strings = []
+    n = 10
+    for index in range(0, len(bodystr), n):
+        split_strings.append(bodystr[index: index + n])
+    return bodystr,split_strings
+
 
 PRACTICE = 2
 EXERCISE = 5
@@ -23,7 +21,7 @@ print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
 IP = "212.128.253.170"
-PORT = 8089
+PORT = 8090
 
 # -- Create a client object
 c = Client(IP, PORT)
@@ -36,8 +34,11 @@ FILENAME = "FRAT1.txt"
 
 # -- Send a message to the server
 print("Sending a message to the server...")
-response1 = "Sending gene U5"
-response2= dividing_seq(FILENAME)
-response_list=[response1,response2]
-for i in response_list:
-    print("To server: ",response1,"\n","From server: ",c.debug_talk(i))
+response1 = "NULL Seq created"
+response2= dividing_seq(FILENAME)[0]
+print("From server: ", c.debug_talk(response1))
+print("From server: ", c.debug_talk(response2))
+for i in dividing_seq(FILENAME)[1]:
+    print("From server: ",c.debug_talk(i))
+    if dividing_seq(FILENAME)[1].index(i)>3:
+        break
--- P2/teacherserver.py
+++ P2/teacherserver.py
@@ -1,7 +1,7 @@
 import socket
 
 # Configure the Server's IP and PORT
-PORT = 8089
+PORT = 8090
 IP = "212.128.253.170"
 MAX_OPEN_REQUESTS = 5
 
