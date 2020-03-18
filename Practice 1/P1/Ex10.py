d52c9d8785da15f8b46cfd4799ecd95e1fd2ef97
j-diazper <jdiazper2001@gmail.com>
2020-03-18 09:59:13 +0100

    changes

--- Practice 1/P1/Ex10.py
+++ Practice 1/P1/Ex10.py
@@ -1,5 +1,5 @@
 from Seq1 import Seq
-Folder = "/home/alumnos/jdiazper/PycharmProjects/2019-2020-PNE-Practices/Practice 1/P1/"
+Folder = r"C:\Users\jesus.diaz\PycharmProjects\2019-2020-PNE-Practices\Practice 1\P1\\"
 GEN_list=["U5","ADA", "FRAT1", "FXN","RNU6_269P"]
 baselist=["A","C","G","T"]
 txt=".txt"
