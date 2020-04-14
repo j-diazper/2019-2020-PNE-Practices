d0380222ca92ea047acaeed7d4dc1f09d701eca5
j-diazper <jdiazper2001@gmail.com>
2020-04-14 15:33:17 +0200

    Finished

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,7 +1,6 @@
 from Seq0 import *
-from pathlib import Path
-Folder= "/home/alumnos/jdiazper/PycharmProjects/2019-2020-PNE-Practices/Session 04/Exercise 1/"
-DNA_file= "U5.txt"
-FILENAME1= Folder + DNA_file
+Folder = "/home/alumnos/jdiazper/PycharmProjects/2019-2020-PNE-Practices/Session 04/Exercise 1/"
+DNA_file = "U5.txt"
+FILENAME1 = Folder + DNA_file
 print("DNA file: ", DNA_file)
-print("The first 20 bases are: \n",seq_read_fasta(FILENAME1))
+print("The first 20 bases are: \n", seq_read_fasta(FILENAME1))
--- P2/Client0.py
+++ P2/Client0.py
@@ -1,42 +1,27 @@
-#Class for sending messages easily to the server
 import socket
-from termcolor import colored
 
 class Client:
-    def __init__(self,IP,PORT):
-        self.IP=IP
-        self.PORT=PORT
+    def __init__(self, IP, PORT):
+        self.IP = IP
+        self.PORT = PORT
     def ping(self):
-        print("OK!")
+        print("OK!\n")
         return self
     def __str__(self):
-        return(f"Connection to SERVER at {self.IP}, PORT: {self.PORT}")
-    def talk(self,msg):
-        # -- Create the socket
+        return (f"Connection to SERVER at {self.IP}, PORT: {self.PORT}")
+    def talk(self, msg):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # establish the connection to the Server (IP, PORT)
         s.connect((self.IP, self.PORT))
-
-        # Send data.
         s.send(str.encode(msg))
-
-        # Receive data
         response = s.recv(2048).decode("utf-8")
-
-        # Close the socket
         s.close()
-
-        # Return the response
         return response
-    def debug_talk(self,msg):
+    def debug_talk(self, msg):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self.IP, self.PORT))
-        msg= colored(msg,"blue")
+        msg = (msg, "green")
         s.send(str.encode(msg))
         resp = s.recv(2048).decode("utf-8")
-        response= colored(resp,"green")
+        response =(resp, "green")
         s.close()
         return response
-
-
