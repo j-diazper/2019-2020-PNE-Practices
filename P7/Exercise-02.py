e8115d7c809750272a0e98db96401b4b758f6654
j-diazper <jdiazper2001@gmail.com>
2020-04-22 13:33:36 +0200

    Finished

--- P7/Exercise-02.py
+++ P7/Exercise-02.py
@@ -1,20 +1,11 @@
-GENES = {
-    'FRAT1': 'ENSG00000165879',
-    'ADA': 'ENSG00000196839',
-    'FXN': 'ENSG00000165060',
-    'RNU6_269P': 'ENSG00000212379',
-    'MIR633': 'ENSG00000207552',
-    'TTTY4C': 'ENSG00000228296',
-    'RBMY2YP': 'ENSG00000227633',
-    'FGFR3': 'ENSG00000068078',
-    'KDR': 'ENSG00000128052',
-    'ANK2': 'ENSG00000145362',
-}
+gene_list= {'FRAT1': 'ENSG00000165879','ADA': 'ENSG00000196839','FXN': 'ENSG00000165060','RNU6_269P': 'ENSG00000212379',
+'MIR633': 'ENSG00000207552','TTTY4C': 'ENSG00000228296','RBMY2YP': 'ENSG00000227633','FGFR3': 'ENSG00000068078',
+'KDR': 'ENSG00000128052','ANK2': 'ENSG00000145362'}
 
-print()
+
 print("Dictionary of Genes!")
-print("There are", len(GENES),"genes in the dicctionary:")
+print("There are", len(gene_list),"genes in the dicctionary:")
 
-for gene in GENES:
+for gene in gene_list:
     print(gene, end="")
-    print(": -->", GENES[gene])
+    print(": -->", gene_list[gene])
